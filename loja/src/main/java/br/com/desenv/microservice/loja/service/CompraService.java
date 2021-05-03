7d3c8cf745ef3f8bbaf249f9f3a7d0258c473648
Anderson Rodrigues <maikbru@gmail.com>
2021-05-03 14:23:33 -0300

    papertrail log

--- loja/src/main/java/br/com/desenv/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/desenv/microservice/loja/service/CompraService.java
@@ -4,17 +4,24 @@ import br.com.desenv.microservice.loja.client.FornecedorClient;
 import br.com.desenv.microservice.loja.dto.CompraDto;
 import br.com.desenv.microservice.loja.dto.InfoPedidoDto;
 import br.com.desenv.microservice.loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompraService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
     @Autowired
     private FornecedorClient fornecedorClient;
 
     public Compra realizaCompra(CompraDto compra) {
-        InfoFornecedorDto infoFornecedorDto = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+        final String estado = compra.getEndereco().getEstado();
+        LOG.info("buscando informações do fornecedor de {}", estado);
+        InfoFornecedorDto infoFornecedorDto = fornecedorClient.getInfoPorEstado(estado);
+        LOG.info("localizando um pedido");
         InfoPedidoDto pedidoDto = fornecedorClient.realizaPedido(compra.getItens());
         Compra compraSalva = new Compra();
         compraSalva.setPedidoId(pedidoDto.getId());
